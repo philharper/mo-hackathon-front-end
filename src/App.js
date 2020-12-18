8a7893e3763530ea79f60536a6fc003cfb337dc4
Phil Harper <philiph@motabilityoperations.co.uk>
2020-12-18 11:13:47 +0000

    Log body

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const hirokubase = 'https://mo-hackathon.herokuapp.com/'
 
 const getHello = () => {
   fetch(hirokubase, {method: 'GET'}).then(response => {
-    console.log(response);
+    console.log(response.body);
     return response;
   }).catch(error => {
     console.log(error);
